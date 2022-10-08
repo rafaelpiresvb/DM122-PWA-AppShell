5cbce37260a6b34d28a5d7717e81b4825604ba78
Rafael Vilas Boas <rafaelpiresvb@gmail.com>
2022-10-08 16:06:58 -0300

    feat: display offline page when no internet connection

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
-self.addEventListener('install', () => {
+const staticCache = 'appShell-v1';
+
+const assetsToCache = ["offline.html"];
+
+async function cacheStaticAssets() {
+    const cache = await caches.open(staticCache);
+    return cache.addAll(assetsToCache);
+}
+
+async function networkFirst(request) {
+    try {
+        return await fetch(request);
+    } catch (error) {
+        console.log("[Service Worker] network error", error);
+        const cache = await caches.open(staticCache);
+        return cache.match('offline.html');
+    }
+}
+
+self.addEventListener('install', (event) => {
     console.log('[Service Worker] Installing Service Worker');
+    event.waitUntil(cacheStaticAssets());
     self.skipWaiting();
 });
 
@@ -10,5 +30,5 @@ self.addEventListener('activate', () => {
 
 self.addEventListener('fetch', (event) => {
     console.log('[Service Worker] Fetch event...', event.request);
-    event.respondWith(fetch(event.request.url));
+    event.respondWith(networkFirst(event.request));
 });
