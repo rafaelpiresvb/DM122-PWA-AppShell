8f860b86070398dc0392ebc442c3658c0a00fa7a
Rafael Vilas Boas <rafaelpiresvb@gmail.com>
2022-10-08 14:49:27 -0300

    feat: fetch event

--- sw.js
+++ sw.js
@@ -7,3 +7,8 @@ self.addEventListener('activate', () => {
     console.log('[Service Worker] Activating Service Worker');
     return self.clients.claim();
 });
+
+self.addEventListener('fetch', (event) => {
+    console.log('[Service Worker] Fetch event...', event.request);
+    event.respondWith(fetch(event.request.url));
+});
